345a563a5dd70b85866a86617cfd5bbbe398aa79
karatugo <karatugo@gmail.com>
2020-06-13 21:45:57 +0300

    Init Star Wars API project

---
+++ automated-spoon/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoading: false,
+            character: {}
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        fetch("https://swapi.dev/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    isLoading: false,
+                    character: data
+                })
+            })
+    }
+
+    render() {
+        const text = this.state.isLoading ? "Loading..." : this.state.character.name
+        return (
+            <div>
+                {text}
+            </div>
+        )
+    }
+}
+
+export default App;
